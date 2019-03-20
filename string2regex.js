e4178a0902b8b55c2da95368e218a89e52aa2a44
Richie Bendall <richiebendall@gmail.com>
2019-03-20 23:45:00 +1300

    Added file

---
+++ string2regex.js
@@ -0,0 +1,12 @@
+const String2Regex = str => {
+  // Main regex
+  const main = str.match(/\/(.+)\/.*/)[1]
+
+  // Regex options
+  const options = str.match(/\/.+\/(.*)/)[1]
+
+  // Return compiled regex
+  return new RegExp(main, options)
+}
+
+export default GGScript
